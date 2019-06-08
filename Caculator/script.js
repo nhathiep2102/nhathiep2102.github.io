1896239120fedfe355cb2c477ead62222d990ab3
nhathiep2102 <49982783+nhathiep2102@users.noreply.github.com>
2019-06-08 16:53:41 +0700

    first commit

---
+++ Caculator/script.js
@@ -0,0 +1,41 @@
+//press
+function press(button){
+	document.getElementById('result').value += button.innerHTML;
+}
+
+//Ket qua tinh
+function equal(){
+	document.getElementById('result').value = eval(result.value);
+}
+
+//Bình Phương
+function bp(){
+	document.getElementById('result').value = result.value * result.value;
+}
+
+//Căn bậc 2:
+function cbh(){
+	document.getElementById('result').value = Math.sqrt(result.value);
+}
+
+//Tính sin:
+function tinhsin(){
+	document.getElementById('result').value = Math.sin(result.value);
+}
+
+//Tính cos:
+function tinhcos(){
+	document.getElementById('result').value = Math.cos(result.value);
+}
+
+//Nút xóa:
+function back(){
+	var x = result.value;
+	document.getElementById('result').value = x.substring(0, x.length - 1);
+}
+
+//AC:
+function clean(){
+	document.getElementById('result').value = "";
+}
+
