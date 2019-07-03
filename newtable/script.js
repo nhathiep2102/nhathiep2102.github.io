6ef6530cceb65b141539b7bb19aa6ce8537f5217
nhathiep2102 <49982783+nhathiep2102@users.noreply.github.com>
2019-07-03 15:34:40 +0700

    first commit

---
+++ newtable/script.js
@@ -0,0 +1,23 @@
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        let content = "";
+        var data = JSON.parse(this.response);
+    for (var i = 0; i < data.length; i++) {
+        content +=
+        `<tr>
+        <td><img src = ${data[i].img}></td> 
+        <td> ${data[i].name} </td> 
+        <td> ${data[i].series}</td>
+        <td> ${data[i].price} </td> 
+        <td> ${data[i].amount} </td>
+        </tr>`;
+    }
+     document.getElementById("myTbody").innerHTML = content;
+    }
+  }
+  xhttp.open("GET", "https://raw.githubusercontent.com/nhathiep2102/nhathiep2102.github.io/master/newtable/product.json", true);
+  xhttp.send();
+}
