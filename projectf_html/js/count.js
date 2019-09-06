64d0fa1cb97c6decb1888aab1b37739d5bba3c6f
nhathiep <nhathiep.2102@gmail.com>
2019-09-06 22:00:19 +0700

    oke

---
+++ projectf_html/js/count.js
@@ -0,0 +1,11 @@
+$('.counter').each(function () {
+    var size = $(this).text().split(".")[1] ? $(this).text().split(".")[1].length : 0;
+    $(this).prop('Counter', 0).animate({
+       Counter: $(this).text()
+    }, {
+       duration: 5000,
+       step: function (func) {
+          $(this).text(parseFloat(func).toFixed(size));
+       }
+    });
+ });
